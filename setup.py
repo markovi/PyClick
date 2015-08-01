11a1f6b00daad2684814dd01147856125942ceb8
Ilya Markov <i.markov@uva.nl>
2015-08-01 10:54:01 +0200

    Add task_centric package to installation script.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='PyClick',
     version='0.2',
-    packages=['pyclick', 'pyclick.search_session', 'pyclick.click_models', 'pyclick.utils'],
+    packages=['pyclick', 'pyclick.search_session', 'pyclick.click_models', 'pyclick.click_models.task_centric', 'pyclick.utils'],
     url='https://bitbucket.org/markil/pyclick2',
     license='GNU General Public License',
     author='Ilya Markov',
